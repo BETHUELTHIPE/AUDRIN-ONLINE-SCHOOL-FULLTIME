b9938593ab6735cba3d5f7f8728fc4037516b4db
Bethuel <moukangwebethuel@gmail>
2024-04-07 18:22:00 +0200

    Connect database locally

--- config/settings.py
+++ config/settings.py
@@ -108,16 +108,16 @@ ASGI_APPLICATION = "config.asgi.application"
 # so consider using postgresql instead
 # Database settings
 # Database settings
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': config("DB_NAME"),
-        #'USER': config("DB_USER"),
-        #'PASSWORD': config("DB_PASSWORD"),
-        #'HOST': config("DB_HOST"),
-        #'PORT': config("DB_PORT", cast=int),
-    #}
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT", cast=int),
+    }
+}
 
 
 

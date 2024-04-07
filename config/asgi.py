325f926f703c2530c5b2ccdc5200019412c04274
Bethuel <moukangwebethuel@gmail>
2024-04-07 19:41:44 +0200

    pip install channels

--- config/asgi.py
+++ config/asgi.py
@@ -1,15 +1,8 @@
 import os
 import django
-
 from channels.routing import ProtocolTypeRouter
-from channels.routing import AsgiHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 
-application = ProtocolTypeRouter(
-    {
-        "http": AsgiHandler(),
-        # Just HTTP for now. (We can add other protocols later.)
-    }
-)
+application = ProtocolTypeRouter({})
